5ca3b7ec47de38650a4a5bbe96b702f43d30fc42
prathap poojari <72817845+PRATHAPA-POOJARI@users.noreply.github.com>
2023-11-05 19:23:05 +0530

    Update Signup.jsx

--- client/src/assets/Signup.jsx
+++ client/src/assets/Signup.jsx
@@ -5,6 +5,32 @@ function Signup() {
             <div className="bg-white p-3 rounded w-25">
                 <h2>Register</h2>
 
+               <form>
+                <div className="mb-3">
+                    <label htmlFor="email">
+                        <strong>Name</strong>
+                    </label>
+                    <input
+                    type='text'
+                    placeholder="Enter a name"
+                    autoComplete="off"
+                    name="email"
+                    className="form-control rounded-0" />
+                    </div>
+
+                    <div className="mb-3">
+                    <label htmlFor="email">
+                        <strong>Name</strong>
+                    </label>
+                    <input
+                    type='text'
+                    placeholder="Enter a name"
+                    autoComplete="off"
+                    name="email"
+                    className="form-control rounded-0" />
+                    </div>
+                    
+                 </form>
             </div>
         </div>
     )
