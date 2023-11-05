150c54860312064b94b06f897b791054e96d4a15
prathap poojari <72817845+PRATHAPA-POOJARI@users.noreply.github.com>
2023-11-05 18:42:14 +0530

    Create Signup.jsx

---
+++ client/src/assets/Signup.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+function Signup() {
+    return (
+        <div>
+            <div className="bg-white p-3 rounded w-25">
+                <h2>Register</h2>
+
+            </div>
+        </div>
+    )
+}
