b2eebf7fafb85312607511274df23a9baaa3e1b3
prathap poojari <72817845+PRATHAPA-POOJARI@users.noreply.github.com>
2023-11-10 21:43:00 +0530

    just connect mongodb

--- server/index.js
+++ server/index.js
@@ -1,7 +1,14 @@
 const  express=require('express')
 const mongoose=require('mongoose');
 const cors= require('cors');
+const  EmployeeModel=require('./models/Employee')
 const app =express;
 app.use(express.json());
 app.use(cors());
-mongoose.connect()
+mongoose.connect("mongodb://127.0.0.1/employee:27017");
+app.post('/register',(req,res)=>{
+
+})
+app.lisen(3000,()=>{
+    console.log("server is running")
+})
